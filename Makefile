1370340a676df2d0b4fdb3f264ed841259e32b1b
auerbach <clarence.auerbach-amoros@etu.u-paris.fr>
2025-11-01 17:38:59 +0100

    création du processus demon

--- Makefile
+++ Makefile
@@ -8,11 +8,14 @@ all : erraid tadmor
 src/bin:
 	mkdir -p src/bin
 
-erraid : src/main/erraid.c | src/bin
+erraid : src/main/erraid.c | src/bin 
 	$(CC) $(CFLAGS) src/main/erraid.c -o src/bin/erraid
 
-tadmor : src/main/tadmor.c | src/bin
+tadmor : src/main/tadmor.c | src/bin 
 	$(CC) $(CFLAGS) src/main/tadmor.c -o src/bin/tadmor
 
-distclean:
+distclean :
 	rm -f src/bin/*
+
+run : 
+	mkdir -p /tmp/$(USER)/erraid/tasks && ./src/bin/erraid $(USER)
--- src/main/erraid.c
+++ src/main/erraid.c
@@ -1,8 +1,34 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include<unistd.h>
+
+#define RUN_DIRECTORY "../test/exemple-arborescence-1/tmp-username-erraid/tasks"
 
 int main()
 {
-    printf("Deamon process \n");
+
+    pid_t child_pid = fork(); 
+    assert(child_pid != -1);
+    if( child_pid > 0 ) exit(EXIT_SUCCESS); // the parent process ends
+    
+    setsid(); // new processes will get the current PID (I think)
+    child_pid = fork(); 
+    assert(child_pid != -1);
+    if( child_pid > 0 ) exit(EXIT_SUCCESS); 
+
+    
+
+    while(true)
+    {  
+    /*  To stop the process get PID with ps aux | grep erraid and kill PID */
+        printf("Deamon process \n");
+        sleep(1);
+    }
 
     return 0;
 }
