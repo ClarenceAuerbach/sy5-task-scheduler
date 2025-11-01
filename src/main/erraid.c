c11409a140ac24e2f720d3027bf957482e6c7f3b
auerbach <clarence.auerbach-amoros@etu.u-paris.fr>
2025-11-01 18:47:32 +0100

    test creation de fichier log et ecriture dessus

--- src/main/erraid.c
+++ src/main/erraid.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,9 +8,9 @@
 #include <time.h>
 #include<unistd.h>
 
-#define RUN_DIRECTORY "../test/exemple-arborescence-1/tmp-username-erraid/tasks"
+char RUN_DIRECTORY[4096] ;
 
-int main()
+int main(int argc, char *argv[])
 {
 
     pid_t child_pid = fork(); 
@@ -20,15 +21,49 @@ int main()
     child_pid = fork(); 
     assert(child_pid != -1);
     if( child_pid > 0 ) exit(EXIT_SUCCESS); 
-
     
+    if( argc != 2 ) {
+        printf( "Usage : use ´make run´ or pass $USER as an argument\n" );
+        exit( EXIT_SUCCESS );
+    }
+    
+    snprintf(RUN_DIRECTORY, 5+4096+13,"%s%s%s", "/tmp/", argv[1],"/erraid/tasks");
+
+    char fpath_stdout[4096];
+    char fpath_stderr[4096];
+    snprintf(fpath_stdout, 4096+7,"%s%s", RUN_DIRECTORY,"/stdout");
+    snprintf(fpath_stderr, 4096+7,"%s%s", RUN_DIRECTORY,"/stderr");
+
+    int fd_out, fd_err ;
+    if( (fd_out = open( fpath_stdout , O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR)) < 0 ) {
+        printf( "Couldn't open fd_out\n" );
+        exit( EXIT_SUCCESS );
+    }
+    if( (fd_err = open( fpath_stderr , O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
+        printf( "Couldn't open fd_err\n" );
+        exit( EXIT_SUCCESS );
+    }
+
+    // Optional: redirects stdout/stderr of the daemon
+    dup2(fd_out, STDOUT_FILENO);
+    dup2(fd_err, STDERR_FILENO);
+    
+    char * buf = "Out message from deamon\nwith error\n" ;
 
     while(true)
     {  
     /*  To stop the process get PID with ps aux | grep erraid and kill PID */
-        printf("Deamon process \n");
+        
+        write(fd_out, buf, 24);
+        fsync(fd_out); // forces flush
+
+        write(fd_err, buf, 35);
+        fsync(fd_err); 
+        
         sleep(1);
     }
 
+    close(fd_out);
+    close(fd_err);
     return 0;
 }
