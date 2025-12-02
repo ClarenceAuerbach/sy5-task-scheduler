0d443fffc2df7b8223f0579b3ac0515499813ccc
Louis Rialland <louis.rialland@etu.u-paris.fr>
2025-12-02 21:57:23 +0100

    Added actually useful str_util functions

--- include/str_util.h
+++ include/str_util.h
@@ -3,12 +3,26 @@
 
 #include <stddef.h>
 
-// Concatenates the `count` strings in strings
-// Result must be freed
-char *concat_n(size_t n, char *strings[]);
+// Capacity: bytes allocated for this string
+// Size: number of characters in the string (not including null byte)
+// Data: heap allocated data
+typedef struct {
+    char *data;
+    size_t length;
+    size_t capacity;
+} string_t;
 
-// Concatenates two strings
-// Result must be freed
-char *concat(char *s1, char *s2);
+// Builds a string (heap allocated data!) from a char
+string_t *new_string(char *s);
+
+// Appends a char* to a string
+// Returns NULL on error
+int append(string_t *dest, char *s);
+
+// Removes n characters from the string
+void truncate_by(string_t *str, int n);
+
+// Removes characters such that the length of the string is n
+void truncate_to(string_t *str, int n);
 
 #endif
--- src/main/str_util.c
+++ src/main/str_util.c
@@ -4,26 +4,58 @@
 
 #include "str_util.h"
 
-char *concat_n(size_t count, char *strings[]) {
-    size_t res_len = 0;
-    for (size_t i = 0; i < count; i++) {
-        res_len += strlen(strings[i]);
+string_t *new_string(char *s) {
+    string_t *res = malloc(sizeof(string_t));
+    if (!res) return NULL;
+
+    size_t len = strlen(s);
+    res->length = len;
+    res->capacity = len+1;
+
+    res->data = malloc(len+1);
+    if (!res->data) {
+        free(res);
+        return NULL;
     }
-    char *res = malloc(res_len+1);
-    char *ptr = res;
-    for (size_t i = 0; i < count; i++) {
-        strcpy(ptr, strings[i]);
-        ptr += strlen(strings[i]);
-    }
+    memcpy(res->data, s, len);
+    res->data[len]='\0';
+
     return res;
 }
 
-char *concat(char *s1, char *s2) {
-    size_t res_len = strlen(s1) + strlen(s2) + 1;
-    char *res = malloc(res_len);
-    char *ptr = res;
-    strcpy(ptr, s1);
-    ptr += strlen(s1);
-    strcpy(ptr, s2);
-    return res;
+void free_string(string_t *string) {
+    if (string) {
+        if (string->data) free(string->data);
+        free(string);
+    }
+}
+
+int append(string_t *dest, char *s) {
+    size_t src_len = strlen(s);
+    size_t min_capacity = dest->length + src_len + 1;
+    if (dest->capacity < min_capacity) {
+        size_t new_capacity = 2 * dest->capacity;
+        if (new_capacity < min_capacity) new_capacity = min_capacity;
+        char *new_data = realloc(dest->data, new_capacity);
+        if (!new_data) return -1;
+        dest->data = new_data;
+        dest->capacity = new_capacity;
+    }
+
+    memcpy(dest->data + dest->length, s, src_len+1);
+    dest->length += src_len;
+
+    return 0;
+}
+
+// Removes n characters from the string
+void truncate_by(string_t *str, int n) {
+    str->data[str->length - n] = '\0';
+    str->length -= n;
+}
+
+// Removes characters such that the length of the string is n
+void truncate_to(string_t *str, int n) {
+    str->data[n] = '\0';
+    str->length = n;
 }
